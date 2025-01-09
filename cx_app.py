f0f6dfed59a6fed4eb2467128da865d603a45b32
lupalom2024 <luis.palomino.pe@outlook.com>
2025-01-09 18:33:16 -0500

    Add files via upload

--- cx_app.py
+++ cx_app.py
@@ -1,5 +1,6 @@
 
 import streamlit as st
+import streamlit as st1
 import pyodbc
 import pandas as pd
 from utils.database import *
@@ -8,7 +9,7 @@ import plotly.express as px
 
 st.set_page_config(page_title="Dashboard CX", layout="wide")
 
-st.title("Dashboard CX")
+#st.title("Dashboard CX")
 
 st.markdown("""
             <style>
@@ -21,36 +22,62 @@ st.markdown("""
 st.sidebar.title("Portal")
 st.sidebar.markdown("---")
 
-inventory_trend_btn = st.sidebar.button("Inventory Trend")
+psaz_data_btn = st.sidebar.button("CX BC Psaz")
+uploaded_file = st.file_uploader("Please choice the data file", type="csv", key=1)
 
-print("paso 1")
+#col1, col2 = st.columns(2)
+#with col1:
+if uploaded_file is not None:
+    st.header("CX BC Psaz")
+    st.write(" ") 
 
-#if inventory_trend_btn:
-col1, col2, col3 = st.columns(3)
-with col1:
-    uploaded_file = st.file_uploader("Please choice the data file", type="csv")
+    try:
+        @st.cache_data
+        def load_csv(uploaded_file):
+            return pd.read_csv(uploaded_file)
 
-col1, col2 = st.columns(2)
-with col1:
-    if uploaded_file is not None:
-        df = pd.read_csv(uploaded_file)
+        df = load_csv(uploaded_file)
+        st.write("Preview data file (Last 10 records):")
+        st.dataframe(df.tail(10))
+        required_columns = ["Period", "OTBSpend"]
+        filtered_df = df[required_columns]
+
         try:
-            df.set_index("Nombre",inplace=True)
-            df_transpuesta = df.T 
-            st.title("Inventory Trend")
-            st.write(" ") 
-            st.line_chart(df_transpuesta)
+            if "Period" not in df.columns:
+                st.error("Error file.")
+            else:                 
+                grouped_data = filtered_df.groupby("Period").sum()
+#                st.line_chart(grouped_data)
         except Exception as e:
             st.error(f"Error to processing data: {e}")
+    except Exception as e:
+        st.error(f"Error to processing data: {e}")
 
-with col2:
-    if uploaded_file is not None:
-        df_melted = pd.melt(df, id_vars="Nombre", var_name="Mes", value_name="Valor")
-        fig = px.line(
-            df_melted,
-            x="Mes",
-            y="Valor",
-            color="Nombre",
-            markers=True,
-            labels={"Valor": "Valor", "Mes": "Mes", "Nombre": "Línea"}
-            )
+st.sidebar.markdown("---")
+st.sidebar.write("")
+
+inventory_trend_btn = st.sidebar.button("Inventory Trend")
+uploaded_file = st.sidebar.file_uploader("Please choice the data file", type="csv", key=2)
+
+#col1, col2 = st.columns(2)
+#with col1:
+if uploaded_file is not None:
+    st.title("Inventory Trend")
+    st.write(" ") 
+    try:
+        @st.cache_data
+        def load_csv(uploaded_file):
+            return pd.read_csv(uploaded_file)
+
+        df = load_csv(uploaded_file)
+        st.write("Preview data file (Last 10 records):")
+        st.dataframe(df.tail(10))
+        try:
+            df = df.sort_values(by="Nombre")
+            df.set_index("Nombre",inplace=True)
+            df_transpuesta = df.T 
+#            st.line_chart(df_transpuesta)
+        except Exception as e:
+            st.error(f"Error to processing data: {e}")
+    except Exception as e:
+        st.error(f"Error to processing data: {e}")
